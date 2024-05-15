a56ff24cb4361c34c0a4c33b117e5ced1cbb3afe
AndresR <119755933+rangertmz@users.noreply.github.com>
2024-05-15 14:47:20 -0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import 'dotenv/config'
+
+
+const app = express();
+
+app.use(express.json());
+
+//Example route
+//app.use('/api', productsRouter)
+
+//Error Returns
+app.use((req, res, next) => {
+    res.send({error:next})
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Listening on port ' + process.env.PORT)
+})
